98bc1caf7e83d5032516959773e132aee5630430
Fofone9 <mr.paa2004@gmail.com>
2024-03-08 17:26:55 +0300

    стиль сделал

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ const example = {
     ],
     blocks:{
         '1':{
-            name:"награда за результат научно-исследовательской работы, проводимой студентом",
+            name:"Награда за результат научно-исследовательской работы, проводимой студентом1",
             configuration:{
                 "НИР":[0,0,0,0],
                 "Конфференция":[3,3,3,3]
             }
         },
         '2':{
-            name:"награда за результат научно-исследовательской работы, проводимой студентом2",
+            name:"Награда за результат научно-исследовательской работы, проводимой студентом2",
             configuration:{
                 "НИР":[0,0,0,0],
                 "Конфференция":[3,3,3,3]
             }
         },
         '3':{
-            name:"награда за результат научно-исследовательской работы, проводимой студентом1",
+            name:"Награда за результат научно-исследовательской работы, проводимой студентом3",
             configuration:{
                 "НИР":[0,0,0,0],
                 "Конфференция":[3,3,3,3]
@@ -48,7 +48,7 @@ for (block in example['blocks'])
 // создание таблицы
 {
     const table = document.createElement('table');
-
+    table.className = "table";
 // создание заголовка таблицы
     const caption = document.createElement("caption");
     caption.className = 'table_caption';
@@ -57,11 +57,14 @@ for (block in example['blocks'])
 
     // создание заголовков столбцов таблицы
     const tableHead = document.createElement("thead")
+    tableHead.className = "table_header"
     const box = document.createElement('td')
-        box.innerHTML = 'мероприятие';
-        tableHead.append(box)
+    box.className = 'first_row_elem';
+    box.innerHTML = 'мероприятие';
+    tableHead.append(box)
     for(let i in levels){
         const box = document.createElement('td')
+        box.className = 'row_elem';
         box.innerHTML = levels[i];
         tableHead.append(box)
     }
@@ -74,11 +77,13 @@ for (block in example['blocks'])
         const tableLine = document.createElement('tr');
         tableLine.className='table_line';
         const box = document.createElement('td');
+        box.className = 'first_row_elem';
         box.innerHTML = key;
         tableLine.append(box);
         let elems = lines[key]
         elems.forEach(element => {
             const box = document.createElement('td');
+            box.className = 'row_elem';
             box.innerHTML = `<input class = "input ${key}" value = ${element} />`;
             tableLine.append(box);
         });
